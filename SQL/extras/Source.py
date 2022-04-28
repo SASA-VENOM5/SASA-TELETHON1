84861d41d0bc88af97921d8ae1be24c66cb4287b
SASA-VENOM5 <104542521+SASA-VENOM5@users.noreply.github.com>
2022-04-28 16:26:52 +0200

    Add files via upload

---
+++ SQL/extras/Source.py
@@ -0,0 +1,11 @@
+import re
+import time
+from datetime import datetime
+from userbot import StartTime, iqthon
+from userbot.Config import Config
+from userbot.plugins import mention
+help1 = ("**𓆩𓆩⦙ كـيفـيه التـنصيـب:𓆪𓆪**")
+help2 = ("**𓆩𓆩⦙ قـائـمـه الاوامــر:𓆪𓆪**\n**𓆩𓆩⦙ مـطـوريـن الـسـورس:𓆪𓆪** @M_2_F 𖠄 @alw_sh313 ** ")
+TG_BOT = Config.TG_BOT_USERNAME
+TM = time.strftime("%I:%M")
+Sour = f"**.𓆩𓆩: الـاصـدار 7.5 𓆪𓆪.** \n.𓆩𓆩: انـت  {mention} 𓆪𓆪. \n**.𓆩𓆩: الـوقـت {TM} 𓆪𓆪.**\n**.𓆩𓆩: بـوتـي {TG_BOT} 𓆪𓆪.**\n**.𓆩𓆩: الـمـطـوريـن : @M_2_F 𖠄 @alw_sh313 𓆪𓆪.**"
